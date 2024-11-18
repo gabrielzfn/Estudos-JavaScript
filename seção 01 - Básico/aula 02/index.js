33ed54563756178a5bbff3f5659af8b5b4281e17
Gabriel Zefino Teixeira <gabriel.zefino@outlook.com>
2024-11-18 10:41:00 -0300

    The basic of comments

---
+++ seção 01 - Básico/aula 02/index.js
@@ -0,0 +1,11 @@
+// Comentário básico (ignorado na exibição)
+console.log('abcd');
+
+
+// Comentário de bloco (normalmente utilizada para créditos ou explicações)
+/* 
+- Linha de comentário 1
+- Linha de comentário 2
+- Linha de comentário 3
+*/
+console.log('abcd');
