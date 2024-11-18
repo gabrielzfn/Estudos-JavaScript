c03db13f099d78cf01febe8a6802ed5733df3d5f
Gabriel Zefino Teixeira <gabriel.zefino@outlook.com>
2024-11-18 11:22:30 -0300

    add 'JS and navigator' and rename folders

---
+++ seção 01 - Básico/aula 03 - JS and navigator/index.js
@@ -0,0 +1,6 @@
+// Colocando o caminho dentro da tag SCRIPT no html, irá aparecer na página o script que queremos (NÃO RECOMENDADO)
+// - Pressionando CTRL+u conseguimos olhar aquilo que está por trás
+// - Podemos manipular o que está presente na página através do navegador
+
+// Um comentário aleatório
+console.log('abcd');
