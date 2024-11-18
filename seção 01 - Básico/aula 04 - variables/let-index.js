3f6136e87555b09277863caa67b680ffa3bbe95c
Gabriel Zefino Teixeira <gabriel.zefino@outlook.com>
2024-11-18 14:15:04 -0300

    Add 'let' variable and rules

---
+++ seção 01 - Básico/aula 04 - variables/let-index.js
@@ -0,0 +1,17 @@
+// O 'var' faz a mesma coisa que o 'let', porém é mais antigo e pouco se utiliza
+// O 'let' 'e variável, ou seja, toda mudança resulta em uma alteração
+let nome1 = 'João';
+
+console.log(nome1, 'nasceu em 1984');
+console.log('Em 2000', nome1, 'conheceu Maria');
+console.log(nome1, 'casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com', nome1, 'em 2015');
+console.log('O filho de', nome1, 'se chama Eduardo');
+
+
+// Podemos alterar o valor da variável, a qual irá apresentar o novo valor a partir da mudança
+nome1 = 'Paulo';
+
+console.log(nome1, 'testes');
+
+
---
+++ seção 01 - Básico/aula 04 - variables/rules.md
@@ -0,0 +1,5 @@
+#### Regras das variáveis:
+
+- Não podemos criar variáveis com palavras reservadas: **let console**
+- Variáveis precisam ter nomes significativos
+- Não pode criar variáveis com inicial numérica: **1nome**
