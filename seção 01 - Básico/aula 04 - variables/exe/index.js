dd05b51219205ff31272dec41c7b9b9959318d3c
Gabriel Zefino Teixeira <gabriel.zefino@outlook.com>
2024-11-18 15:21:19 -0300

    Resolve exercise

---
+++ seção 01 - Básico/aula 04 - variables/exe/index.js
@@ -0,0 +1,15 @@
+const nome  = 'Luiz Otávio Miranda';
+let idade   = 30;
+let peso    = 84;
+let altura  = 1.80;
+let imc;
+let nasceu;
+
+
+imc = peso / (altura + altura)
+nasceu = 2019 - idade
+
+
+console.log(`${nome} tem ${idade} aos, pesa ${peso}kg`);
+console.log(`tem ${altura} de altura e seu IMC é de ${imc}`);
+console.log(`${nome} nasceu em ${nasceu}`)
