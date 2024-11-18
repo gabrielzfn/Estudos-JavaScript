aa454d008d7d507a368dafb83598a331f67ca03b
Gabriel Zefino Teixeira <gabriel.zefino@outlook.com>
2024-11-18 10:19:59 -0300

    The basic of 'console.log'

---
+++ seção 01 - Básico/aula 01/index.js
@@ -0,0 +1,9 @@
+// 1. Exibição de mensagem básica:
+// -  Ponto e vírgula são opcionais no JavaScript
+// -  Aspas simples não podem ser utilizadas duplamente duas vezes na mesma frase, assim como as duplas
+// -  Sinal de crase "`" pode ser utilizada para fazer TEMPLATE STRINGS
+console.log("mensagem aleatória"); 
+
+// 2. Exibição de números básico:
+// -  Separação de número com casas decimais é por "."
+console.log(123, 15.8)
--- seção 01 - Básico/index.js
+++
@@ -1,1 +0,0 @@
-console.log(`mensagem aleatória`);
