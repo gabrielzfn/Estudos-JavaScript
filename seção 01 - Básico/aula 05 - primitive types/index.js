bd523b0e5987234b91c47718c4127b6fb25120af
Gabriel Zefino Teixeira <gabriel.zefino@outlook.com>
2024-11-19 08:47:14 -0300

    Add primitive types

---
+++ seção 01 - Básico/aula 05 - primitive types/index.js
@@ -0,0 +1,16 @@
+// 1. String
+const nome1 = 'Fulano';
+const nome2 = "Ciclano";
+const nome3 = `Beltrano`;
+
+// 2. Number
+const num1 = 10;
+const num2 = 1.5;
+
+// 3. Undefined (Não aponta para lugar algum na memória)
+let nome4; // declaração sem valor algum
+const nome5 = null; // apontando para null/nada. Usa-se quando não queremos nenhum valor definitivamente
+
+// 4. Booleans
+const aprovado1 = true
+const aprovado2 = false
