129dc3835125114465a6e169f5bbb46bb637908f
Gabriel Zefino Teixeira <gabriel.zefino@outlook.com>
2024-11-22 14:28:52 -0300

    Math in navigator

---
+++ seção 01 - Básico/aula 07 - navigator/js/index.js
@@ -0,0 +1,11 @@
+let num1 = prompt('Digite o primeiro número: ');
+let num2 = prompt('Digite o segundo número: ');
+
+// Fazer a conversão para número
+num1 = Number(num1);
+num2 = Number(num2);
+
+
+// Fazer a soma dos dois números
+const sum = num1+num2;
+alert(`O resultado da soma é: ${sum}`);
