d964ac38574554555cc96843f4c6486f86463510
Gabriel Zefino <gabrielzt12@hotmail.com>
2024-11-24 17:29:01 -0300

    Add 'more about strings'

---
+++ seção 01 - Básico/aula 08 - more about strings/index.js
@@ -0,0 +1,5 @@
+let umaString = "Uma string";
+
+console.log(umaString.match(/[a-z]/)); // Expressão regular para encontrar todas as letras minúsculas dessa string
+console.log(umaString.search(/[t]/)); // Expressão regular para procurar uma letra
+console.log(umaString.replace(/["Uma", "Outra"]/)); // Expressão para substituir uma palavra por outra
